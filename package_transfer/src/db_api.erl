09aa06eda182759cee3da5a3c6747ce376abc419
Eli Murdock <elimurdock85@gmail.com>
2024-06-03 09:16:58 -0600

    remove friends

--- package_transfer/src/db_api.erl
+++ package_transfer/src/db_api.erl
@@ -1,10 +1,6 @@
 -module(db_api).
--export([put_friends_for/3, put_package/3, get_package/2]).
+-export([put_package/3, get_package/2]).
 
-
-put_friends_for(Name_key,Friends,Pid)->
-	Request=riakc_obj:new(<<"friends">>, Name_key, Friends),
-	riakc_pb_socket:put(Pid, Request).
 
 put_package(Package_ID, Location_ID, Pid) ->
 	Request=riakc_obj:new(<<"package">>, Package_ID, Location_ID),

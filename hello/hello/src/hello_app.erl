40c0405eedd3da5398ed17674552572ed1b166b7
Thomas Jamieson <jamiesonthomas314@gmail.com>
2024-06-05 09:37:50 -0600

    grumpy

--- hello/hello/src/hello_app.erl
+++ hello/hello/src/hello_app.erl
@@ -23,7 +23,7 @@ start(_Type, _Args) ->
     	%tls stands for transport layer security
           {ok,_} = cowboy:start_tls(https_listener, [
                   		{port, 443},
-				{certfile, PrivDir ++ "hello/hello/priv/ssl/fullchain.pem"},
+				{certfile, "tracker_business_logic/hello/hello/priv/ssl/fullchain.pem"},
 				{keyfile, PrivDir ++ "hello/hello/priv/ssl/privkey.pem"}
               		], #{env => #{dispatch => Dispatch}}),
 

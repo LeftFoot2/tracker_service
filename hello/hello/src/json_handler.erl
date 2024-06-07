0b58aaa34b7db0d44a6c714bd55c5c2d88790b9e
rony.richard11 <rony.richard11@gmail.com>
2024-06-07 13:15:11 -0600

    JSON Testing

--- hello/hello/src/json_handler.erl
+++ hello/hello/src/json_handler.erl
@@ -1,8 +1,6 @@
-%% @doc JSON handler.
 -module(json_handler).
 
 -export([init/2]).
-
 
 init(Req0, Opts) ->
     {Method, Req1} = cowboy_req:method(Req0),
@@ -11,29 +9,23 @@ init(Req0, Opts) ->
             {ok, Body, Req2} = cowboy_req:read_body(Req1),
             try jsx:decode(Body, [return_maps]) of
                 #{<<"name">> := Name} ->
-                    Message = io_lib:format("My name is ~s", [binary_to_list(Name)]),
+                    % Convert binary "Name" to list for response (if necessary)
                     Req3 = cowboy_req:reply(200,
-                        #{
-                            <<"content-type">> => <<"text/plain; charset=utf-8">>
-                        },
-                        Message,
+                        #{<<"content-type">> => <<"text/plain; charset=utf-8">>},
+                        Name,  % Send back the name directly
                         Req2),
                     {ok, Req3, Opts}
             catch
                 _:_ ->
                     Req3 = cowboy_req:reply(400,
-                        #{
-                            <<"content-type">> => <<"text/plain; charset=utf-8">>
-                        },
+                        #{<<"content-type">> => <<"text/plain; charset=utf-8">>},
                         "Invalid JSON data.",
                         Req2),
                     {ok, Req3, Opts}
             end;
         _ ->
             Req2 = cowboy_req:reply(405,
-                #{
-                    <<"content-type">> => <<"text/plain; charset=utf-8">>
-                },
+                #{<<"content-type">> => <<"text/plain; charset=utf-8">>},
                 "Method Not Allowed",
                 Req1),
             {ok, Req2, Opts}

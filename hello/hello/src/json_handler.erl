954004be0ecc970d6be27c8152ab7dc8d96b65c2
rony.richard11 <rony.richard11@gmail.com>
2024-06-06 20:11:53 -0600

    JSON Testing

--- hello/hello/src/json_handler.erl
+++ hello/hello/src/json_handler.erl
@@ -1,24 +1,39 @@
+%% @doc JSON handler.
 -module(json_handler).
+
 -export([init/2]).
 
 init(Req0, Opts) ->
-    Method = cowboy_req:method(Req0),
+    {Method, Req1} = cowboy_req:method(Req0),
     case Method of
         <<"POST">> ->
-            {ok, Body, Req1} = cowboy_req:read_body(Req0),
-            JsonData = jsx:decode(Body, [return_maps]),
-            Name = maps:get(<<"name">>, JsonData, <<"">>),
-            Email = maps:get(<<"email">>, JsonData, <<"">>),
-            Status = maps:get(<<"status">>, JsonData, <<"">>),
-            Message = io_lib:format("My name is ~s and my email is ~s, I am an ~s user.", [Name, Email, Status]),
-            Req2 = cowboy_req:reply(200,
-                #{
-                    <<"content-type">> => <<"text/plain">>
-                },
-                Message,
-                Req1),
-            {ok, Req2, Opts};
+            {ok, Body, Req2} = cowboy_req:read_body(Req1),
+            try jsx:decode(Body, [return_maps]) of
+                #{<<"name">> := Name} ->
+                    Message = io_lib:format("My name is ~s", [binary_to_list(Name)]),
+                    Req3 = cowboy_req:reply(200,
+                        #{
+                            <<"content-type">> => <<"text/plain; charset=utf-8">>
+                        },
+                        Message,
+                        Req2),
+                    {ok, Req3, Opts}
+            catch
+                _:_ ->
+                    Req3 = cowboy_req:reply(400,
+                        #{
+                            <<"content-type">> => <<"text/plain; charset=utf-8">>
+                        },
+                        "Invalid JSON data.",
+                        Req2),
+                    {ok, Req3, Opts}
+            end;
         _ ->
-            Req1 = cowboy_req:reply(405, #{}, <<"Method Not Allowed">>, Req0),
-            {ok, Req1, Opts}
+            Req2 = cowboy_req:reply(405,
+                #{
+                    <<"content-type">> => <<"text/plain; charset=utf-8">>
+                },
+                "Method Not Allowed",
+                Req1),
+            {ok, Req2, Opts}
     end.

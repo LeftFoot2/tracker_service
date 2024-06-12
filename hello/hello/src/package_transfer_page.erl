06cda725f4e0dc659ebecdde2014625af2b01986
Thomas Jamieson <jamiesonthomas314@gmail.com>
2024-06-12 10:01:57 -0600

    changed package_trasfer_page to send data via erpc to the business_logic module

--- hello/hello/src/package_transfer_page.erl
+++ hello/hello/src/package_transfer_page.erl
@@ -1,10 +1,26 @@
-%% @doc Hello world handler.
+%% @doc package transfer handler.
 -module(package_transfer_page).
 
 -export([init/2]).
 
+% init(Req0, Opts) ->
+%         Req = cowboy_req:reply(200, #{
+%                 <<"content-type">> => <<"text/plain">>
+%         }, "Hello world this is exciting! Test", Req0),
+%         {ok, Req, Opts}.
 init(Req0, Opts) ->
-        Req = cowboy_req:reply(200, #{
-                <<"content-type">> => <<"text/plain">>
-        }, "Hello world this is exciting! Test", Req0),
-        {ok, Req, Opts}.
+        {ok,Data,_} = cowboy_req:read_body(Req0),
+                %it is expected that the data consists of one quoted-string name
+                %in an array.
+                [Package_ID,Location_ID] = jsx:decode(Data),
+                Result = erpc:call("deliveryman@thomasjamiesonprograms.com/package_transferred", business_logic, package_transfer_url_handler, [Package_ID,Location_ID]), %get_friends_server:get_friends_of(Package_ID) of
+                        %         {error,notfound} -> "no such person";
+                        %         Friends -> Friends
+                        % end,
+                        
+                Encoded_message = jsx:encode(Result),
+                %io:format("~p~n",[get_friends_server:get_friends_of(Name)]),
+                Response = cowboy_req:reply(200, #{
+                        <<"content-type">> => <<"text/json">>
+                }, Encoded_message, Req0),
+                {ok, Response, Opts}.

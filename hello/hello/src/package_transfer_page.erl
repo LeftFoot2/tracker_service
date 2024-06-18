6fb0d19ffe248a5a4ecf37cea234d62d8dc2a548
Eli Murdock <elimurdock85@gmail.com>
2024-06-18 12:49:24 -0600

    format

--- hello/hello/src/package_transfer_page.erl
+++ hello/hello/src/package_transfer_page.erl
@@ -58,25 +58,26 @@ init(Req0, Opts) ->
 %         {ok, Req, Opts}.
 % init(Req0, Opts) ->
         {ok,Data,_} = cowboy_req:read_body(Req0),
-                % it is expected that the data consists of one quoted-string name
-                % in an array.
-                Req = cowboy_req:reply(200, #{
-                             <<"content-type">> => <<"text/plain">>
-                     }, "Hello world this is exciting! Test", Req0),
-                     {ok, Req, Opts},
-
-                     [{<<"Package_ID">>,Package_ID},{<<"Location_ID">>,Location_ID}]  = jsx:decode(Data),
-                {<<"35">>,<<"14">>} = {Package_ID,Location_ID},
-                Result = erpc:call('deliveryman@bl.thomasjamiesonprograms.com', business_logic, package_transfer_url_handler, [Package_ID,Location_ID]), 
+        % it is expected that the data consists of one quoted-string name
+        % in an array.
+        Req = cowboy_req:reply(200, 
+                #{<<"content-type">> => <<"text/plain">>},
+                "Hello world this is exciting! Test", Req0),
+        {ok, Req, Opts},
+        
+        [{<<"Package_ID">>,Package_ID},{<<"Location_ID">>,Location_ID}]  = jsx:decode(Data),
+        [<<"35">>,<<"14">>] = [Package_ID,Location_ID],
+        Result = erpc:call('deliveryman@bl.thomasjamiesonprograms.com', business_logic, package_transfer_url_handler, [Package_ID,Location_ID]), 
         %get_friends_server:get_friends_of(Package_ID) of
-                        %         {error,notfound} -> "no such person";
-                        %         Friends -> Friends
-                        % end,
-                        
-                Encoded_message = jsx:encode(Result),
-                %io:format("~p~n",[get_friends_server:get_friends_of(Name)]),
-                Response = cowboy_req:reply(200, #{
-                        <<"content-type">> => <<"text/json">>
-                }, Encoded_message, Req0),
-                {ok, Response, Opts}.
+                %         {error,notfound} -> "no such person";
+                %         Friends -> Friends
+                % end,
+                
+        Encoded_message = jsx:encode(Result),
+        %io:format("~p~n",[get_friends_server:get_friends_of(Name)]),
+
+        Response = cowboy_req:reply(200,
+        #{<<"content-type">> => <<"text/json">>},
+        Encoded_message, Req0),
+        {ok, Response, Opts}.
 

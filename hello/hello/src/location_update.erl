bce349386059ab1baf9c0f8c65954b037737cb52
Thomas Jamieson <jamiesonthomas314@gmail.com>
2024-06-24 10:21:41 -0600

    changed patteren match

--- hello/hello/src/location_update.erl
+++ hello/hello/src/location_update.erl
@@ -4,8 +4,8 @@
 
 init(Req0, Opts) ->
         {ok,Data,_} = cowboy_req:read_body(Req0),
-
-        [{<<"Location_ID">>,Location_ID}, {<<"Coordinates">>,{Latitude, Longitude}}] = jsx:decode(Data),
+        [{<<"Location_ID">>, Location_ID},
+        {<<"Coordinates">>, [{<<"Latitude">>, Latitude}, {<<"Longitude">>, Longitude}]}] = jsx:decode(Data),
         Result = erpc:call('deliveryman@bl.thomasjamiesonprograms.com', business_logic, put_location, [Location_ID,{Latitude,Longitude}]), 
         
         Encoded_message = jsx:encode(Result),

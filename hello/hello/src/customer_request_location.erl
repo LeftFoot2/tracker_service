af41ee085f5a8acb091b17d4cb46dc3c7cd20e23
Thomas Jamieson <jamiesonthomas314@gmail.com>
2024-07-17 09:37:27 -0600

    again

--- hello/hello/src/customer_request_location.erl
+++ hello/hello/src/customer_request_location.erl
@@ -11,10 +11,10 @@ init(Req0, Opts) ->
     %         "Hello world this is exciting! Test", Req0),
     % {ok, Req, Opts},
     
-    <<"package_1">>  = jsx:decode(Data),
-    [Package_Id]  = jsx:decode(Data),
+    % <<"package_1">>  = jsx:decode(Data),
+    Package_ID  = jsx:decode(Data),
     % test [<<"35">>,<<"14">>] = [Package_ID,Location_ID],
-    Result = erpc:call('deliveryman@bl.thomasjamiesonprograms.com', business_logic, location_request, ["Package_ID"]),
+    Result = erpc:call('deliveryman@bl.thomasjamiesonprograms.com', business_logic, location_request, [Package_ID]),
     
     %get_friends_server:get_friends_of(Package_ID) of
             %         {error,notfound} -> "no such person";
